bc14e9231556fd0477b73066fd8344b631ecc9be
RinachanBoard31 <04y1708c01anshincommittee@gmail.com>
2024-06-24 09:47:26 +0900

    time stamp add for backend

--- server/src/main.go
+++ server/src/main.go
@@ -27,6 +27,7 @@ import (
 	"log"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -39,18 +40,23 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+type Message struct {
+	timestamp time.Time
+	message   string
+}
+
 type Server struct {
 	clients   map[*websocket.Conn]bool
-	broadcast chan string
-	messages  []string
+	broadcast chan Message
+	messages  []Message
 	mu        sync.Mutex
 }
 
 func NewServer() *Server {
 	return &Server{
 		clients:   make(map[*websocket.Conn]bool),
-		broadcast: make(chan string),
-		messages:  make([]string, 0),
+		broadcast: make(chan Message),
+		messages:  make([]Message, 0),
 	}
 }
 
@@ -68,7 +74,8 @@ func (s *Server) handleClients(w http.ResponseWriter, r *http.Request) {
 
 	// これまでのメッセージ履歴を新しいClientに送信
 	for _, msg := range s.messages {
-		if err := conn.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+		fmtMsg := fmt.Sprintf("%s: %s", msg.timestamp.Format(time.RFC3339), msg.message)
+		if err := conn.WriteMessage(websocket.TextMessage, []byte(fmtMsg)); err != nil {
 			log.Println(err)
 			return
 		}
@@ -85,30 +92,35 @@ func (s *Server) handleClients(w http.ResponseWriter, r *http.Request) {
 			s.mu.Unlock()
 			break
 		}
-		log.Println("Received message: ", string(msg))
+		newMsg := Message{
+			timestamp: time.Now(),
+			message:   string(msg),
+		}
+		log.Printf("%s Received: %s", newMsg.timestamp.Format(time.RFC3339), newMsg.message)
 		// 全てのメッセージを履歴に保存
 		s.mu.Lock()
-		s.messages = append(s.messages, string(msg))
+		s.messages = append(s.messages, newMsg)
 		s.mu.Unlock()
 		// 他の全てのClientにメッセージを送信
-		s.broadcast <- string(msg)
+		s.broadcast <- newMsg
 	}
 }
 
 func (s *Server) handleMessages() {
 	// メッセージを待ち受け、全てのClientに送信
 	for {
-		msg := <-s.broadcast
+		newMsg := <-s.broadcast
 		s.mu.Lock()
+		fmtMsg := fmt.Sprintf("%s: %s", newMsg.timestamp.Format(time.RFC3339), newMsg.message)
 		for client := range s.clients {
-			if err := client.WriteMessage(websocket.TextMessage, []byte(msg)); err != nil {
+			if err := client.WriteMessage(websocket.TextMessage, []byte(fmtMsg)); err != nil {
 				log.Println(err)
 				client.Close()
 				return
 			}
 		}
 		s.mu.Unlock()
-		log.Println("Sent messageto all clients: ", string(msg))
+		log.Println("Sent messageto all clients: ", string(newMsg.message))
 	}
 }
 
