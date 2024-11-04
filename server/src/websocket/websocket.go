7e31375831a3c6e6da89fca753d0c1203b1dff30
RinachanBoard31 <04y1708c01anshincommittee@gmail.com>
2024-11-04 22:57:44 +0900

    comment out

--- server/src/websocket/websocket.go
+++ server/src/websocket/websocket.go
@@ -264,7 +264,7 @@ func (s *Server) handleSmilePoint(message Message) {
 		s.level = 4
 	case s.totalSmilePoint >= 500: // 500
 		s.level = 3
-	case s.totalSmilePoint >= 200: // 200
+	case s.totalSmilePoint >= 200: // 200 // ここを動的に
 		s.level = 2
 	}
 	s.mu.Unlock()
